e1e502c3c5bc4b947fe84091270747df5fa42091
Connor Wilson <connormwilson@gmail.com>
2020-05-05 01:00:43 -0600

    added controlers to routes

--- api/models/product.js
+++ api/models/product.js
@@ -4,7 +4,8 @@ const schema = mongoose.Schema;
 const productSchema = schema({
     _id: schema.Types.ObjectId,
     name: { type: String, required: true },
-    price: { type: Number, required: true }
+    price: { type: Number, required: true },
+    productImage: { type: String }
 });
 
 module.exports = mongoose.model('Product', productSchema);
